d43551ae9e9c2349a0e19f9524544c5a7d769c1a
Terry Bu <burea1124@gmail.com>
2016-04-28 19:06:39 -0400

    Initial Commit

---
+++ TimeHopAssignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeHopAssignment
+//
+//  Created by Terry Bu on 4/28/16.
+//  Copyright © 2016 Terry Bu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
